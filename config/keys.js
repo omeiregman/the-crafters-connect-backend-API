53cfc86b77e54007a419af7830a2cca0154b4b75
omeiregman <omeiregodsman@gmail.com>
2018-07-03 00:59:49 +0100

    jwt authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:admin1111@ds125331.mlab.com:25331/craftersconnectdb'
+  mongoURI: 'mongodb://admin:admin1111@ds125331.mlab.com:25331/craftersconnectdb',
+  secretOrKey: 'jwtsecret'
 }
